8e6f973d0227132b74ca5423a1927ee356cf5136
Luca Anna Palasti <54719967+lucap1211@users.noreply.github.com>
2023-09-16 01:16:05 +0000

    Finished analysis for Anchorage

---
+++ class_projects/tmp.py
@@ -0,0 +1,6 @@
+# Define URL for Anchorage mean annual temperature data and format url
+anc_temp_url = (
+    'https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/city/'
+    'time-series/USW00026451/tavg/12/12/1954-2023.csv?base_prd=true&'
+    'begbaseyear=1991&endbaseyear=2020')
+anc_temp_url
